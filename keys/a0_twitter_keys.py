36569de8c6e5e23dbb4a6cb9403ce8b1ee3ced59
alvinmingguw <48747769+alvinmingguw@users.noreply.github.com>
2020-10-27 16:41:57 +0700

    Update a0_twitter_keys.py

--- keys/a0_twitter_keys.py
+++ keys/a0_twitter_keys.py
@@ -1,4 +1,4 @@
-consumer_key = "7Da6Ywu29JKj4d7dGgLvzlFia"
-consumer_secret = "T3klRLrZWU8QdiZtTcc79EmeKlmad03ePIpdmMy53TgOWaIRAn"
-access_token = "75456326-hfdPFsPK5Obe0UvuTDkfYCAIpU1DGIO79IFe3DQwz"
-access_token_secret = "aTF6zLxeZvQlCHWBUuMYjqwhYv1FUUY4WFSdsQvZJOEAe"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
